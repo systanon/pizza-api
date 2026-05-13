4fb987353735d4de8d95fa6b18df5e021f08608b
Serhii <tustanovskiis@gmail.com>
2026-05-13 15:20:46 +0200

    feat: add router

---
+++ router.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"pizza-backend/internal/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter(
+	productHandler *handler.ProductHandler,
+	categoryHandler *handler.CategoryHandler,
+
+) *gin.Engine {
+	r := gin.Default()
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/categories", categoryHandler.GetCategoryList)
+
+		v1.GET("/products", productHandler.GetProductList)
+		v1.GET("/products/:id", productHandler.GetProductByID)
+	}
+	r.NoRoute(handler.NotFoundHandler)
+
+	return r
+}
