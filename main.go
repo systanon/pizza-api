b54069ff1c7aa6096b7e040d2d8d9a03aeff80e2
Serhii <tustanovskiis@gmail.com>
2026-06-02 13:07:49 +0200

    feat: implement order

--- main.go
+++ main.go
@@ -23,20 +23,23 @@ func main() {
 	categoryRepo := repository.NewCategoryRepo(db)
 	addonRepo := repository.NewAddonRepo(db)
 	cartRepo := repository.NewCartRepo(db)
+	orderRepo := repository.NewOrderRepo(db)
 
 	// services
 	productService := service.NewProductService(productRepo)
 	addonService := service.NewAddonService(addonRepo)
 	categoryService := service.NewCategoryService(categoryRepo)
 	cartService := service.NewCartService(cartRepo, productRepo)
+	orderService := service.NewOrderService(orderRepo, cartRepo)
 
 	// handlers
 	productHandler := handler.NewProductHandler(productService)
 	addonHandler := handler.NewAddonHandler(addonService)
 	categoryHandler := handler.NewCategoryHandler(categoryService)
 	cartHandler := handler.NewCartHandler(cartService)
+	orderHandler := handler.NewOrderHandler(orderService)
 
 	// router
-	r := NewRouter(productHandler, categoryHandler, addonHandler, cartHandler)
+	r := NewRouter(productHandler, categoryHandler, addonHandler, cartHandler, orderHandler)
 	r.Run(cfg.Addr)
 }
